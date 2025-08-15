cef679935f5db96ae6ef136249c55a6cc08f79fa
Bogdan Kalitenko <kalitenkobv@gmail.com>
2025-08-15 15:52:32 +0500

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Bogdan Kalitenko on 15.08.2025.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
